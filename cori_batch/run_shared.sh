90394eff7622ea321e5e01b95d45361a7e20f23f
Fernando Torales - Acosta <fernando@cori08.nersc.gov>
2020-04-03 12:41:46 -0700

    checking cori batch

--- cori_batch/run_shared.sh
+++ cori_batch/run_shared.sh
@@ -2,6 +2,6 @@
 #SBATCH --image=docker:ddixit/fun4all:eicresearch
 #SBATCH --qos=shared
 #SBATCH --constraint=haswell
-#SBATCH --time=5:00:00
-#SBATCH --array=0-40
-shifter ./sPHENIX_shifter.sh $SLURM_ARRAY_TASK_ID 250
+#SBATCH --time=2:30:00
+#SBATCH --array=0-9
+shifter ./sPHENIX_shifter.sh $SLURM_ARRAY_TASK_ID 100
