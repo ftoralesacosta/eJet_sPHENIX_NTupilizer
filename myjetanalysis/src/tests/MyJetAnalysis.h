14ccc9ef753ff25a8a643d5eee902b145529fcc7
Fernando Torales - Acosta <fernando@cori08.nersc.gov>
2020-06-16 19:04:09 -0700

    Added electron veto and ALL truth jet branches (with veto)

---
+++ myjetanalysis/src/tests/MyJetAnalysis.h
@@ -0,0 +1,128 @@
+#ifndef MYJETANALYSIS_H
+#define MYJETANALYSIS_H
+
+#include <fun4all/SubsysReco.h>
+
+#include <memory>
+#include <string>
+#include <utility>  // std::pair, std::make_pair
+
+#include <g4eval/SvtxClusterEval.h>
+
+#if ! defined(__CINT__) || defined(__CLING__)
+#include <array>
+#endif  // #ifndef __CINT__
+
+class PHCompositeNode;
+class JetEvalStack;
+class TTree;
+class TH1;
+class Jet;
+class JetMap;
+class JetTruthEval;
+//class SvtxTruthEval;
+class SvtxTrackMap;
+
+/// \class MyJetAnalysis
+class MyJetAnalysis : public SubsysReco
+{
+	public:
+		MyJetAnalysis(
+				const std::string &recojetname = "AntiKt_Tower_r04",
+				const std::string &truthjetname = "AntiKt_Truth_r04",
+				const std::string &outputfilename = "myjetanalysis.root");
+
+		virtual ~MyJetAnalysis();
+
+		//! set eta range
+		void
+			setEtaRange(double low, double high)
+			{
+				m_etaRange.first = low;
+				m_etaRange.second = high;
+			}
+		//! set eta range
+		void
+			setPtRange(double low, double high)
+			{
+				m_ptRange.first = low;
+				m_ptRange.second = high;
+			}
+
+		int Init(PHCompositeNode *topNode);
+		int InitRun(PHCompositeNode *topNode);
+		int process_event(PHCompositeNode *topNode);
+		int End(PHCompositeNode *topNode);
+
+	Jet* max_truth_jet_by_track_fastsim(Jet* recojet);
+	int get_track_fastsim_contribution(Jet* recojet, Jet* truthjet);
+	SvtxClusterEval* get_cluster_eval() { return &_clustereval; }
+
+	  /// get a copy of the lower level eval and its memory cache
+	  JetTruthEval* get_truth_eval() { return &_jettrutheval; }
+
+
+	private:
+
+#if ! defined(__CINT__) || defined(__CLING__)
+
+		std::string _truthjetname;
+		JetMap* _truthjets;
+		SvtxClusterEval _clustereval;
+		SvtxTrackMap* _trackmap;
+		JetTruthEval _jettrutheval;
+
+		//! cache the jet evaluation modules
+		std::shared_ptr<JetEvalStack> m_jetEvalStack;	
+
+		std::string m_recoJetName;
+		std::string m_truthJetName;
+		std::string m_outputFileName;	
+
+		//! eta range
+		std::pair<double, double> m_etaRange;
+
+		//! pT range
+		std::pair<double, double> m_ptRange;
+
+		//! max track-jet matching radius
+		double m_trackJetMatchingRadius;
+
+		//! Output histograms
+		TH1 *m_hInclusiveE;
+		TH1 *m_hInclusiveEta;
+		TH1 *m_hInclusivePhi;
+
+		//! Output Tree variables
+		TTree *m_T;
+
+		int m_event;
+		int m_id;
+		int m_nComponent;
+		float m_eta;
+		float m_phi;
+		float m_e;
+		float m_pt;
+
+		int m_truthID;
+		int m_truthNComponent;
+		float m_truthEta;
+		float m_truthPhi;
+		float m_truthE;
+		float m_truthPt;
+
+		//! number of matched tracks
+		int m_nMatchedTrack;
+
+		enum
+		{
+			//! max number of tracks
+			kMaxMatchedTrack = 1000
+		};
+		std::array<float, kMaxMatchedTrack> m_trackdR;
+		std::array<float, kMaxMatchedTrack> m_trackpT;
+
+#endif  // #ifndef __CINT__
+};
+
+#endif  // MYJETANALYSIS_H
