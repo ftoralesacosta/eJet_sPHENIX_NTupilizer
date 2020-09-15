45d06a91d3b80590553e1bae4f4d3709b9cd016e
Fernando Torales - Acosta <fernando@cori08.nersc.gov>
2020-09-14 19:57:46 -0700

    fixed directory change

--- cori_batch/gnu_parallel/parallel_EIC_shifter.sh
+++ cori_batch/gnu_parallel/parallel_EIC_shifter.sh
@@ -3,7 +3,7 @@
 let rando=$RANDOM*$2+$2
 #random number seed uses built in $Random and unique gnu parallel slot number
 
-shifter --image=docker:ddixit/fun4all:eicresearch /project/projectdirs/alice/ftorales/new_Singularity/Singularity/e_Jet_sPHENIX/cori_batch/gnu_parallel_EIC_shifter.sh $1 $2 $rando
+shifter --image=docker:ddixit/fun4all:eicresearch /project/projectdirs/alice/ftorales/new_Singularity/Singularity/e_Jet_sPHENIX/cori_batch/gnu_parallel/gnu_parallel_EIC_shifter.sh $1 $2 $rando
 # First argument = number of events
 # Second argument = gnu parallel slot number for output file name
 # Third argument = random number for MC generator SEED
