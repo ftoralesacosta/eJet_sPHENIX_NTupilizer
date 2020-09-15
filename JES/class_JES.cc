0c70209a3c1d395f5af82f5fba47196d444e8876
Fernando Torales - Acosta <fernando@cori08.nersc.gov>
2020-09-15 16:21:05 -0700

    class_JES updates

--- JES/class_JES.cc
+++ JES/class_JES.cc
@@ -1,7 +1,8 @@
 #include "class_JES.h"
 #include <iostream>
 
-void MyClass::get_binning(std::vector <float> &edges, std::vector <float> &centers,
+
+void MyClass::set_binning(std::vector <float> &edges, std::vector <float> &centers,
 			  float low, float high, float width)
 {
   if (fmod((high-low),width) != 0)
@@ -14,16 +15,16 @@ void MyClass::get_binning(std::vector <float> &edges, std::vector <float> &cente
     centers.push_back(low + width*(0.5+i));
 
   //myobj.get_binning(myobj.pT_bins,myobj.pT_Centers,0,10,2);
+} //pass vectors by reference instead of editing member object to allow for locally defined 
+
+void MyClass::set_pT_binning(float low, float high, float width)
+{
+  set_binning(pT_bins,pT_Centers,low,high,width);
 }
 
-void MyClass::get_pT_binning(float low, float high, float width)
+void MyClass::set_eta_binning(float low, float high, float width)
 {
-  get_binning(pT_bins,pT_Centers,low,high,width);
-}
-
-void MyClass::get_eta_binning(float low, float high, float width)
-{
-  get_binning(eta_bins,eta_Centers,low,high,width);
+  set_binning(eta_bins,eta_Centers,low,high,width);
 }
 
 void MyClass::set_pT_FitRange(float low, float high)
@@ -32,82 +33,145 @@ void MyClass::set_pT_FitRange(float low, float high)
   Fit_pT_Max = high;
 }
 
-void MyClass::make_TH1F_Calib(TString reco_or_corr = "Reconstructed")
-{ 
-  std::vector<TH1F*> pT_Differences;
-  std::vector<TH1F*> pT_Ratios;
-  std::vector<TH1F*> pT_Slices;
-  std::vector<TH1F*> Phi_Deltas;
-  std::vector<TH1F*> Eta_Deltas;
+TString MyClass::set_reco_or_corr(TString input_string)
+{
+  reco_or_corr = input_string;
+}
+
+// TTreeReader MyClass::get_TTree(TString file_name)
+// {//FIXME: make a TTreeReader member variable
+//   std::cout << "Opening: " << file_name << std::endl;
+//   TFile *file = TFile::Open(file_name);
+
+//   if (file == NULL) {
+//     std::cout << " Failed to open "<<file_name<< std::endl;
+//     exit(EXIT_FAILURE);
+//   }
+
+//   TString tree_name = "T";
+//   TTree *_tree_event = dynamic_cast<TTree *>(file->Get("T"));
+
+//   if (_tree_event == NULL) {
+//     std::cout << " Failed to load TTree \""<< tree_name<<"\""<<std::endl;
+//     exit(EXIT_FAILURE);
+//   }
+
+//   TTreeReader The_Tree(_tree_event);
+//   return The_Tree;
+//   //Would like to return pointer, but don't want to make a member variable of TTReader as I want to change it in loops, and one should never return a pointer to a local variable (local in the sense that the scope is within the function).
+
+//   //This is because the variable the pointer points to is out of scope after the function, and this memeroy is freed. But as soon as memory is used, the pointer will point to whatever occupies that memory now.
+
   
-  TString reco_or_corr_pT = TString("p_{T}^{") + reco_or_corr + TString("}");
+//   // TTreeReaderValue<int> njets(The_Tree,"njets");
+//   // TTreeReaderArray<Int_t> JetRecoNConst(The_Tree,"nComponent");
+//   // TTreeReaderArray<Float_t> JetRecoE(The_Tree,"e");
+//   // TTreeReaderArray<Float_t> JetRecopT(The_Tree,"pt");
+
+//   // TTreeReaderValue<int> nAlltruthjets(The_Tree,"nAlltruthjets");
+//   // TTreeReaderArray<Int_t> JetAllTruthNConst(The_Tree,"all_truthNComponent");
+//   // TTreeReaderArray<Float_t> JetAllTruthE(The_Tree,"all_truthE");
+//   // TTreeReaderArray<Float_t> JetAllTruthpT(The_Tree,"all_truthPt");
+// }
+
+std::vector<TH1F*> MyClass::create_TH1F(TString reco_or_corr,TString root_name,
+					  TString title,float *binning)
+{
+  std::vector<TH1F*> TH1F_vector;
+
+  //Internal Root Name.
+  root_name = reco_or_corr + root_name;
+
+  //Histogram title
+  title = title.Insert(title.First("^")+2,reco_or_corr); //insert into latex
   
   for (auto it = pT_bins.begin(); std::next(it,1) != pT_bins.end(); ++it){
-    TString root_name = Form("pT%1.0f_%1.0f",*it*10,*std::next(it,1)*10);
+
+    TString root_range = Form("pT%1.0f_%1.0f",*it*10,*std::next(it,1)*10);
     TString title_range = TString(Form("(%1.2f < p_{T}^{Truth} < %1.2f)",*it,*std::next(it,1)));
 
-    pT_Differences.push_back(new TH1F(reco_or_corr + (TString("_pT_Difference_") + root_name).Data(),
-				      (reco_or_corr_pT + TString(" - p_{T}^{Truth} ")
-				       + title_range).Data(),16,-2,2));
+    TH1F* Single_Histo = new TH1F();
+    Single_Histo->SetName(root_name + root_range);
+    Single_Histo->SetTitle(title + title_range);
+    Single_Histo->SetBins(binning[0],binning[1],binning[2]);
 
-    pT_Ratios.push_back(new TH1F((reco_or_corr + TString("_pT_Ratio_") + root_name).Data(),
-				 (reco_or_corr_pT + TString(" / p_{T}^{Truth} ")
-				  + title_range).Data(),16,-2,2));
-    
-    pT_Slices.push_back(new TH1F((reco_or_corr + TString("_pT_Slices") + root_name).Data(),
-				 (reco_or_corr_pT + title_range).Data(),16,-2,2));
-
-    Phi_Deltas.push_back(new TH1F((reco_or_corr + TString("_delta_phi_") + root_name).Data(),
-				  (TString("#varphi^{") + reco_or_corr+ TString("} - #varphi^{Truth}")
-				   + title_range).Data(),16,-2,2));
-
-    Eta_Deltas.push_back(new TH1F((reco_or_corr + TString("_delta_eta_") + root_name).Data(),
-				  (TString("#eta^{") + reco_or_corr+ TString("} - #eta^{Truth}")
-				   + title_range).Data(),16,-2,2));
-
+    TH1F_vector.push_back(Single_Histo);
   }
-  vectors_TH1F_Calib.push_back(pT_Differences);
-  vectors_TH1F_Calib.push_back(pT_Ratios);
-  vectors_TH1F_Calib.push_back(pT_Slices);
-  vectors_TH1F_Calib.push_back(Phi_Deltas);
-  vectors_TH1F_Calib.push_back(Eta_Deltas);
+  return TH1F_vector;
 }
 
-void MyClass::make_TH2F(TString reco_or_corr = "Reconstructed")
+std::vector<TH2F*> MyClass::create_TH2F(TString reco_or_corr,TString root_name,
+					TString title,float *binning)
 {
+  std::vector<TH2F*> TH2F_vector;
 
-  TString reco_or_corr_pT = TString("p_{T}^{") + reco_or_corr + TString("}");
-  TH2F* H_Diff_pT = new TH2F((TString("2D") + reco_or_corr + TString(pT_Residuals)).Data(),)
+  TString vs_pT = " vs. p_{T}^{Truth}";
+  
+  //Internal Root Name.
+  root_name = reco_or_corr + root_name;
 
-    TH2F* H_Diff_Tpt = new TH2F(Form("%s_H_2D_%s_pT_Residuals",r_c_string.Data(),r_cut_string.Data()),
-                                Form("p_{T}^{%s} - p_{T}^{Truth} vs. p_{T}^{Truth}, %s",r_c_string.Data(),cut_string.Data()),38,0.5,10,50,-10,10);
-    TH2F* H_Ratio_Tpt = new TH2F(Form("%s_H_2D_%s_pT_Ratios",r_c_string.Data(),r_cut_string.Data()),
-                                 Form("p_{T}^{%s} / p_{T}^{Truth} vs. p_{T}^{Truth}, %s",r_c_string.Data(),cut_string.Data()),38,0.5,10,60,0,2);
-    TH2F* H_Reco_Tpt = new TH2F(Form("H_2D_%s_%s_Tpt",r_c_string.Data(),r_cut_string.Data()),
-                                Form("p_{T}^{%s} vs. p_{T}^{Truth}, %s",r_c_string.Data(),cut_string.Data()),38,0.5,10,38,0.5,10);
+  //Histogram title, insert reco/corr for latex
+  title = title.Insert(title.First("^")+2,reco_or_corr);
+  title = title + vs_pT;
+  
+  for (auto it = pT_bins.begin(); std::next(it,1) != pT_bins.end(); ++it){
 
-    H_Diff_Tpt->GetXaxis()->SetTitle("p_{T}^{Truth}");
-    H_Diff_Tpt->GetYaxis()->SetTitle(Form("p_{T}^{%s} - p_{T}^{Truth}",r_c_string.Data()));
+    TString root_range = Form("pT%1.0f_%1.0f",*it*10,*std::next(it,1)*10);
+    TString title_range = TString(Form("(%1.2f < p_{T}^{Truth} < %1.2f)",*it,*std::next(it,1)));
 
-    H_Ratio_Tpt->GetXaxis()->SetTitle("p_{T}^{Truth}");
-    H_Ratio_Tpt->GetYaxis()->SetTitle(Form("#frac{p_{T}^{%s}}{p_{T}^{Truth}}",r_c_string.Data()));
+    TH2F* Single_Histo = new TH2F();
+    Single_Histo->SetName(root_name + root_range);
+    Single_Histo->SetTitle(title + title_range);
+    Single_Histo->SetBins(binning[0],binning[1],binning[2],
+			  binning[0], binning[1], binning[2]);
 
-    H_Reco_Tpt->GetXaxis()->SetTitle("p_{T}^{Truth}");
-    H_Reco_Tpt->GetYaxis()->SetTitle(Form("p_{T}^{%s}",r_c_string.Data()));
-
-    vector_TH2F_Calib.push_back(H_Diff_Tpt);
-    vector_TH2F_Calib.push_back(H_Ratio_Tpt);
-    vector_TH2F_Calib.push_back(H_Reco_Tpt);
+    TH2F_vector.push_back(Single_Histo);
+  }
+  return TH2F_vector;
 }
 
+//void get_calibration()
 
+void MyClass::initialize_histograms(){
 
-int main()
+  float binning[3] = {16,-2,2}; //{nbins,min,max}
+  //FIXME: Pass this as argument. For pT_Slices copy pT truth binning member variable
+  pT_Differences = create_TH1F("Reco","_pT_Difference_","p_{T}^{} - p_{T}^{Truth} ",binning);
+  pT_Ratios = create_TH1F("Reco","_pT_Ratio_","p_{T}^{} / p_{T}^{Truth} ",binning);
+  pT_Slices = create_TH1F("Reco","_pT_Slices","p_{T}^{} ",binning);
+  
+  Phi_Deltas = create_TH1F("Reco","_delta_phi_","#varphi^{} - #varphi^{Truth} ",binning);
+  Eta_Deltas = create_TH1F("Reco","_delta_eta_","#eta^{} - #eta^{Truth} ",binning);
+  
+  Ratio_TH2F_v = create_TH2F("Reco","_pT_Ratio_","p_{T}^{} / p_{T}^{Truth} ",binning);
+  Diff_TH2F_v = create_TH2F("Reco","_pT_Difference_","p_{T}^{} - p_{T}^{Truth} ",binning);
+  Slice_TH2F_v = create_TH2F("Reco","_pT_Slices","p_{T}^{} ",binning);
+}
+
+int main(int argc, char *argv[])
 {
+  if (argc < 2)
+    {
+      std::cout<<"Please Specify the ROOT File: [command] [file]"<<std::endl;
+      exit(EXIT_FAILURE);
+    }
+  
   MyClass myobj;
-  myobj.get_pT_binning(0,10,2);
-  myobj.get_eta_binning(0,10,1);
+  //myobj.get_TTree(TString(argv[1]));
+  //Can loop through argv[i] for multiple files
+
+  TFile *file = TFile::Open(TString(argv[1]));
+  TTreeReader The_Tree("T",file); //T is the Tree name within the file
+  
+  myobj.set_pT_binning(0,10,2);
+  myobj.set_eta_binning(0,10,1);
   myobj.set_pT_FitRange(2,20);
-  myobj.make_TH1F_Calib();
+  //FIXME: Add TEnv capabilit
+
+  myobj.initialize_histograms();
+  
+  
+  //float 2d binning = {idk idk idk idk}
+  //calll myobj.create_TH1F_Calib the same way
   return 0;
 }
--- JES/class_JES.h
+++ JES/class_JES.h
@@ -9,23 +9,12 @@
 #include <TMath.h>
 #include <TTree.h>
 #include <math.h>
-class MyClass {
 
-  //Physics Variables
-  Int_t           m_event;
-  Int_t           id;
-  Int_t           nComponent;
-  Float_t         eta;
-  Float_t         phi;
-  Float_t         e;
-  Float_t         pt;
-  Int_t           truthID;
-  Int_t           truthNComponent;
-  Float_t         truthEta;
-  Float_t         truthPhi;
-  Float_t         truthE;
-  Float_t         truthPt;
-  Int_t           nMatchedTrack;
+#include <TTreeReader.h>
+#include <TTreeReaderValue.h>
+#include <TTreeReaderArray.h>
+
+class MyClass {
 
   //Binning
   std::vector<float> pT_bins;
@@ -36,18 +25,35 @@ class MyClass {
   float Fit_pT_Max;
 
   //JES Histos
-  std::vector<std::vector<TH1F*> > vectors_TH1F_Calib;
-  std::vector<TH2F*> vector_TH2F_Calib;
- 
+  std::vector<TH1F*> pT_Differences;
+  std::vector<TH1F*> pT_Ratios;
+  std::vector<TH1F*> pT_Slices;
+  std::vector<TH1F*> Phi_Deltas;
+  std::vector<TH1F*> Eta_Deltas;
+
+  std::vector<TH2F*> Ratio_TH2F_v;
+  std::vector<TH2F*> Diff_TH2F_v;
+  std::vector<TH2F*> Slice_TH2F_v;
+
+  TString reco_or_corr;
 public:
-  void get_binning(std::vector<float> &edges, std::vector<float> &centers,
+      
+  void set_binning(std::vector<float> &edges, std::vector<float> &centers,
 		   float low, float high, float width);
 
-  void get_pT_binning(float low, float high, float width);
-  void get_eta_binning(float low, float high, float width);
+  void set_pT_binning(float low, float high, float width);
+  void set_eta_binning(float low, float high, float width);
   void set_pT_FitRange(float low, float high);
-  void make_TH1F_Calib(TString reco_or_corr);
-  void make_TH2F(TString reco_or_corr);
+  TString set_reco_or_corr(TString input_string);
+  //FIXME: edit other function to no longer take reco_or.. argument
+  std::vector<TH1F*> create_TH1F(TString reco_or_corr,
+     TString root_name, TString title,float *binning);
+
+  std::vector<TH2F*> create_TH2F(TString reco_or_corr,
+     TString root_name, TString title,float *binning);
+
+  void initialize_histograms();
+
 };
 
-#endif  // __PHOTONJET_H__
+#endif
